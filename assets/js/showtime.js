f24bb3705784480627133764e02fb3f4224f72b4
zulfiachsans <zulfiachsans44@gmail.com>
2022-12-31 12:40:14 +0700

    setup interface ujian and showtime

---
+++ assets/js/showtime.js
@@ -0,0 +1,28 @@
+function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+
+    if (h == 0) {
+        h = 24;
+    }
+    if (h > 24) {
+        h = h - 24;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s;
+
+    document.getElementById("DisplayClock").innerText = time;
+    document.getElementById("DisplayClock").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
